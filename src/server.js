107c91ce24a634d273e2f63c86af151a7cec201d
SvetaSaletska <svitlanagalevich@gmail.com>
2024-08-02 00:23:34 +0300

    config file

---
+++ src/server.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import pino from 'pino-http';
+// import router from './routers/index.js';
+import cors from 'cors';
+
+export const setupServer = () => {
+  const app = express();
+
+  app.use(pino());
+
+  app.use(cors());
+
+  // app.use(router);
+
+  app.get('/', (req, res) => {
+    console.log(undefined.unknown());
+    res.send('Hello, World!');
+  });
+
+  app.use((req, res, next) => {
+    res.status(404).send('Not Found');
+  });
+
+  app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+  });
+};
