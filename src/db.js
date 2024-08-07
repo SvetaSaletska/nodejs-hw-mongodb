029270668509afec878b982c64813ab461e41a41
SvetaSaletska <svitlanagalevich@gmail.com>
2024-08-07 15:14:27 +0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,10 +1,16 @@
 import mongoose from 'mongoose';
+import { env } from '../utils/env.js';
 
 async function initMongoConnection() {
   try {
-    const DB_URI = process.env.MONGODB_URL;
-    await mongoose.conect(DB_URI);
+    const user = env('MONGODB_USER');
+    const pwd = env('MONGODB_PASSWORD');
+    const url = env('MONGODB_URL');
+    const db = env('MONGODB_DB');
 
+    await mongoose.connect(
+      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=mongodb`,
+    );
     console.log('Mongo connection successfully established!');
   } catch (error) {
     console.error(error);
