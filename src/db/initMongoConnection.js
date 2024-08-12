1407924e810a91ac99591823e00535b9362517c7
SvetaSaletska <svitlanagalevich@gmail.com>
2024-08-12 19:32:26 +0300

    u

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -6,11 +6,11 @@ export const initMongoConnection = async () => {
     const user = env('MONGODB_USER');
     const pwd = env('MONGODB_PASSWORD');
     const url = env('MONGODB_URL');
-    const db = env('MONGODB_DB');
+    // const db = env('MONGODB_DB');
 
     await mongoose.connect(
       // `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=mongodb`,
-      `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority&appName=Cluster0`,
+      `mongodb+srv://${user}:${pwd}@${url}/?retryWrites=true&w=majority&appName=Cluster0`,
     );
     console.log('Mongo connection successfully established!');
   } catch (error) {
