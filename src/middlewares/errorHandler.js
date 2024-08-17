4648cd367f69c8d0eae7418ccafa2edb11c60fe8
SvetaSaletska <svitlanagalevich@gmail.com>
2024-08-17 16:53:37 +0300

    update

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,6 +1,6 @@
 import { isHttpError } from 'http-errors';
 
-export const errorHandler = (error, _req, res) => {
+function errorHandler(error, _req, res, _next) {
   if (isHttpError(error) === true) {
     return res.status(error.status).send({
       status: error.status,
@@ -10,6 +10,15 @@ export const errorHandler = (error, _req, res) => {
 
   res.status(500).send({
     status: 500,
-    message: 'Something went wrong',
+    message: 'Internal server error',
   });
-};
+}
+
+export { errorHandler };
+
+// export const errorHandler = (err, req, res, _next) => {
+//   res.status(err.status || 500).json({
+//     status: err.status || 500,
+//     message: err.message || 'Something went wrong',
+//   });
+// };
