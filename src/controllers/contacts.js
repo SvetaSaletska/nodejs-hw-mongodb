572bd9d2e268d2cab955be6c142d08f3aa913def
SvetaSaletska <svitlanagalevich@gmail.com>
2024-08-19 16:55:32 +0300

    update

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -7,7 +7,7 @@ import {
 } from '../services/contacts.js';
 import createHttpError from 'http-errors';
 
-export const getAllContactsController = async (req, res,) => {
+export const getAllContactsController = async (req, res) => {
   const contacts = await getAllContacts();
   res.status(200).json({
     status: 200,
@@ -20,11 +20,12 @@ export const getContactByIdController = async (req, res, next) => {
   const { contactId } = req.params;
   const contact = await getContactById(contactId);
   if (!contact) {
-    next(createHttpError(404, 'Contact not found'));
-    return;
+    // next(createHttpError(404, 'Contact not found'));
+    // return;
+    throw createHttpError(404, 'Contact not found');
   }
 
-  res.status(200).json({
+  res.json({
     status: 200,
     message: `Successfully found contact with id ${contactId}!`,
     data: contact,
