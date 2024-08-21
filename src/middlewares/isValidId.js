1cd9103416fbf01c31c95ec118fb23102aaf142d
SvetaSaletska <svitlanagalevich@gmail.com>
2024-08-21 17:27:42 +0300

    corrections

--- src/middlewares/isValidId.js
+++ src/middlewares/isValidId.js
@@ -2,9 +2,9 @@ import { isValidObjectId } from 'mongoose';
 import createHttpError from 'http-errors';
 
 export const isValidId = (req, res, next) => {
-  const { id } = req.params;
-  if (!isValidObjectId(id)) {
-    throw createHttpError(400, 'Bad Request');
+  const { contactId } = req.params;
+  if (!isValidObjectId(contactId)) {
+    throw createHttpError(400, `${contactId} not valid id`);
   }
 
   next();
