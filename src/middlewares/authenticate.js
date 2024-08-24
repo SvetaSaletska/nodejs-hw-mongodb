3af82ee809e023857fd32e9bc12d0f3c7c550356
SvetaSaletska <svitlanagalevich@gmail.com>
2024-08-24 03:58:47 +0300

    Update authenticate.js

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -45,6 +45,48 @@ export const authenticate = async (req, res, next) => {
   next();
 };
 
+// export const authenticate = async (req, res, next) => {
+//   const authHeader = req.get('Authorization');
+
+//   if (!authHeader) {
+//     next(createHttpError(401, 'Please provide Authorization header'));
+//     return;
+//   }
+
+//   const bearer = authHeader.split(' ')[0];
+//   const token = authHeader.split(' ')[1];
+
+//   if (bearer !== 'Bearer' || !token) {
+//     next(createHttpError(401, 'Auth header should be of type Bearer'));
+//     return;
+//   }
+
+//   const session = await SessionsCollection.findOne({ accessToken: token });
+
+//   if (!session) {
+//     next(createHttpError(401, 'Session not found'));
+//     return;
+//   }
+
+//   const isAccessTokenExpired =
+//     new Date() > new Date(session.accessTokenValidUntil);
+
+//   if (isAccessTokenExpired) {
+//     next(createHttpError(401, 'Access token expired'));
+//   }
+
+//   const user = await UsersCollection.findById(session.userId);
+
+//   if (!user) {
+//     next(createHttpError(401));
+//     return;
+//   }
+
+//   req.user = user;
+
+//   next();
+// };
+
 // export async function authenticate(req, res, next) {
 //   if (typeof req.headers.authorization !== 'string') {
 //     return next(createHttpError(401, 'Please provide Authorization header'));
