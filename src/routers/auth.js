e66090e87e9ea581bd567330302e86feca8d1580
SvetaSaletska <svitlanagalevich@gmail.com>
2024-08-29 20:04:53 +0300

    Update auth.js

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -12,21 +12,21 @@ const authRoutes = express.Router();
 const jsonParser = express.json();
 
 authRoutes.post(
-  '/auth/register',
+  '/register',
   jsonParser,
   validateBody(registerUserSchema),
   ctrlWrapper(registerUserController),
 );
 
 authRoutes.post(
-  '/auth/login',
+  '/login',
   jsonParser,
   validateBody(loginUserSchema),
   ctrlWrapper(loginUserController),
 );
 
-authRoutes.post('/auth/logout', ctrlWrapper(logoutUserController));
+authRoutes.post('/logout', ctrlWrapper(logoutUserController));
 
-authRoutes.post('/auth/refresh', ctrlWrapper(refreshUserSessionController));
+authRoutes.post('/refresh', ctrlWrapper(refreshUserSessionController));
 
 export default authRoutes;
